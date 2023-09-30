cc88648a7f38a434fc4a1710a31639301549de05
EnigmaLabsMx <142854986+EngmLabsMX@users.noreply.github.com>
2023-09-29 22:28:46 -0600

    Update numbers.php

--- Intro/numbers.php
+++ Intro/numbers.php
@@ -1,11 +1,28 @@
-<?php 
+<?php
+// Define a variable $radio and set it to 25.
+$radio = 25;
 
-$radio=25;
+// Define a constant named PI and set it to the value 3.14.
 define('PI', 3.14);
 
-echo PI * ($radio ** 2) ;
+// Calculate and echo the area of a circle using the formula: PI * radius^2
+echo PI * ($radio ** 2);
 
-$radio++;
+// Increase the value of $radio by 5.5.
+$radio += 5.5;
 
-echo "<br>".$radio;
- ?>
+// Echo a line break followed by the updated value of $radio.
+echo "<br>" . $radio;
+
+// Define a variable $num and set it to 34.
+$num = 34;
+
+// Increase the value of $num by 10.
+$num += 10;
+
+// Echo a line break followed by the updated value of $num.
+echo "<br>" . $num;
+
+// Echo a line break followed by the floor value of $radio.
+echo "<br>" . floor($radio);
+?>
